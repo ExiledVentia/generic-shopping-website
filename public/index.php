dc7af21cbc4377e415113694382f24d967a585c5
ricin-u <someone.was.takened@gmail.com>
2024-06-02 13:20:39 +0800

    changed bg-color

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="../css/style.css">
   <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 </head>
-<body class="bg-slate-800">
+<body class="bg-gray-100 dark:bg-gray-800">
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
