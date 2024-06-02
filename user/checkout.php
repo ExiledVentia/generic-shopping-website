bef06c006a6aed4adf33acd0b5870f8f6818404a
ricin-u <someone.was.takened@gmail.com>
2024-06-03 02:48:24 +0800

    checkout was missing the footer

--- user/checkout.php
+++ user/checkout.php
@@ -133,5 +133,13 @@ include '../datefunc.php';
         </table>
     </div>
 </div>
+<!-- foot 🤤🤤🤤 -->
+<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4 ">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Fumo Hideout Doujin Circle</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+<script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
